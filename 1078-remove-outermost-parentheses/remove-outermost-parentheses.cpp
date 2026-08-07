67750a82a3ae81111f929f621bc16e30816d18bd
ankit1337-git <ankitkumar004q@gmail.com>
2026-08-07 19:13:36 +0530

    Time: 0 ms (100.00%) | Memory: 8.8 MB (97.85%) - LeetSync

---
+++ 1078-remove-outermost-parentheses/remove-outermost-parentheses.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    string removeOuterParentheses(string s) {
+        string str = "";
+        int count =0;
+        for(char c: s){
+            if(c == '('){
+                count += 1;
+                if(count > 1){
+                    str += '(';
+                }
+            }else{
+                count -= 1 ;
+                if(count > 0){
+                    str += ')';
+                }
+
+            }
+        }
+        return str;
+        
+    }
+};
